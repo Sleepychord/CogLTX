6f1b8c49fc5571027a6c88d080bab14e63f77a94
Ming Ding <dm_thu@qq.com>
2020-03-18 18:48:27 +0800

    t

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 CAPACITY = 512 # Working Memory
-DEFAULT_MODEL_NAME = 'roberta-base'
+DEFAULT_MODEL_NAME = 'roberta-large'
 BLOCK_SIZE = 63 # The max length of an episode
 BLOCK_MIN = 10 # The min length of an episode
 
